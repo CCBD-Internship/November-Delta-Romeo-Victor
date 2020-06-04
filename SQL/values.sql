e2821f54f999a31a3291c8feb23e5e8cd9c24fe4
niksram <nikhilsram.off@gmail.com>
2020-06-04 23:06:50 +0530

    create4.sql and input.sql sorted now just run both of them, later lets do inspectdb once again

---
+++ SQL/values.sql
@@ -0,0 +1,14 @@
+\c bigbowl;
+
+insert into department values ('computer science');
+
+insert into panel values('AAAA','2020-01-01 00:00:01','false');
+
+insert into faculty values('7474','ecve','xyu7f','auiu@gmail.com','+916565787890','computer science',1);
+
+insert into team values('dofiuj','fxihv','7474',1);
+
+insert into student values('PES1201801947','dcyn','wccvn','cqng@gmail.com','+918382828218','computer science',1);
+insert into student values('PES1201801944','dc wwyn','4dgwccvn','3rf@gmail.com','+918312828218','computer science',1);
+insert into student values('PES1201801941','ewrv4f','d4','gd4@gmail.com','+918322828218','computer science',1);
+insert into student values('PES1201801942','vef','4d','cqng@fgf.com','+918332828218','computer science',1);
