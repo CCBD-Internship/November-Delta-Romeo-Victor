fe6905aeeae0cf7b2910199e7a0c79b1726aecbe
niksram <nikhilsram.off@gmail.com>
2020-06-13 17:32:57 +0530

    models modified a little (panel and team id)

--- Back_end/NVRD/eval/models.py
+++ Back_end/NVRD/eval/models.py
@@ -6,6 +6,8 @@
 #   * Remove `managed = True` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename #db_table values or field names.
 from django.db import models
+from django.utils import timezone
+
 
 class Department(models.Model):
     dept = models.CharField(primary_key=True, max_length=50)
@@ -21,41 +23,57 @@ class Faculty(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=13)
     dept = models.ForeignKey('Department', models.DO_NOTHING)
-    fac_type = models.CharField(max_length=20,choices=[("assistant_prof","assistant_prof"),("associate_prof","associate_prof"),("professor","professor")],default="professor")
+    fac_type = models.CharField(max_length=20, choices=[("assistant_prof", "assistant_prof"), (
+        "associate_prof", "associate_prof"), ("professor", "professor")], default="professor")
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     class Meta:
         managed = True
         constraints = [
-            models.CheckConstraint(name='phone_constraint_faculty',check=models.Q(phone__startswith='+')),
+            models.CheckConstraint(
+                name='phone_constraint_faculty', check=models.Q(phone__startswith='+')),
         ]
         #db_table = 'faculty'
+
 
 class FacultyPanel(models.Model):
     fac_id = models.ForeignKey('Faculty', models.CASCADE)
     panel_id = models.ForeignKey('Panel', models.CASCADE)
     is_coordinator = models.BooleanField(default=False)
+    id = models.AutoField(primary_key=True)
 
     class Meta:
         managed = True
         #db_table = 'faculty_panel'
         unique_together = (('fac_id', 'panel_id'),)
 
+
+# use this to generate values for panel_id
+# def add_one_panel(year_code):
+#     largest = Panel.objects.filter(year_code=year_code).order_by('panel_id').last()
+#     if not largest:
+#         return str(1).zfill(10)
+#     return str(int(largest.panel_id) + 1).zfill(10)
+
 class Panel(models.Model):
-    label = models.CharField(max_length=100)
-    is_active = models.BooleanField(blank=True, null=True,default=True)
+
+    year_code = models.CharField(max_length=10)
+    panel_id = models.CharField(max_length=10)
+    is_active = models.BooleanField(blank=True, null=True, default=True)
+    ctime = models.DateTimeField(default=timezone.now)
     id = models.AutoField(primary_key=True)
-    ctime = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         managed = True
         #db_table = 'panel'
-        unique_together=(('label'),)
+        unique_together = (('year_code', 'panel_id'),)
+
 
 class PanelReview(models.Model):
+
     review_number = models.IntegerField()
-    panel = models.ForeignKey('Panel',models.CASCADE)
+    panel = models.ForeignKey('Panel', models.CASCADE)
     open_time = models.DateTimeField()
     close_time = models.DateTimeField()
     id = models.AutoField(primary_key=True)
@@ -65,9 +83,12 @@ class PanelReview(models.Model):
         #db_table = 'panel_review'
         unique_together = (('panel', 'review_number'),)
 
+
 class Review1(models.Model):
-    srn = models.OneToOneField('Student',models.CASCADE,to_field='srn',primary_key=True)
-    fac_id = models.ForeignKey(Faculty, models.DO_NOTHING)
+    srn = models.OneToOneField(
+        'Student', models.CASCADE, to_field='srn', primary_key=True)
+    fac_id = models.ForeignKey(
+        Faculty, models.DO_NOTHING, null=True, blank=True)
     project_work = models.IntegerField()
     quality_of_demo = models.IntegerField()
     project_report = models.IntegerField()
@@ -79,9 +100,12 @@ class Review1(models.Model):
         #db_table = 'review_1'
         unique_together = (('srn', 'fac_id'),)
 
+
 class Review2(models.Model):
-    srn = models.OneToOneField('Student',models.CASCADE,to_field='srn',primary_key=True)
-    fac_id = models.ForeignKey(Faculty, models.DO_NOTHING)
+    srn = models.OneToOneField(
+        'Student', models.CASCADE, to_field='srn', primary_key=True)
+    fac_id = models.ForeignKey(
+        Faculty, models.DO_NOTHING, null=True, blank=True)
     project_work = models.IntegerField()
     quality_of_demo = models.IntegerField()
     project_report = models.IntegerField()
@@ -93,9 +117,12 @@ class Review2(models.Model):
         #db_table = 'review_2'
         unique_together = (('srn', 'fac_id'),)
 
+
 class Review3(models.Model):
-    srn = models.OneToOneField('Student',models.CASCADE,to_field='srn',primary_key=True)
-    fac_id = models.ForeignKey(Faculty, models.DO_NOTHING)
+    srn = models.OneToOneField(
+        'Student', models.CASCADE, to_field='srn', primary_key=True)
+    fac_id = models.ForeignKey(
+        Faculty, models.DO_NOTHING, null=True, blank=True)
     project_work = models.IntegerField()
     quality_of_demo = models.IntegerField()
     project_report = models.IntegerField()
@@ -107,9 +134,12 @@ class Review3(models.Model):
         #db_table = 'review_3'
         unique_together = (('srn', 'fac_id'),)
 
+
 class Review4(models.Model):
-    srn = models.OneToOneField('Student',models.CASCADE,to_field='srn',primary_key=True)
-    fac_id = models.ForeignKey(Faculty, models.DO_NOTHING)
+    srn = models.OneToOneField(
+        'Student', models.CASCADE, to_field='srn', primary_key=True)
+    fac_id = models.ForeignKey(
+        Faculty, models.DO_NOTHING, null=True, blank=True)
     project_work = models.IntegerField()
     quality_of_demo = models.IntegerField()
     project_report = models.IntegerField()
@@ -121,9 +151,12 @@ class Review4(models.Model):
         #db_table = 'review_4'
         unique_together = (('srn', 'fac_id'),)
 
+
 class Review5(models.Model):
-    srn = models.OneToOneField('Student',models.CASCADE,to_field='srn',primary_key=True)
-    fac_id = models.ForeignKey(Faculty, models.DO_NOTHING)
+    srn = models.OneToOneField(
+        'Student', models.CASCADE, to_field='srn', primary_key=True)
+    fac_id = models.ForeignKey(
+        Faculty, models.DO_NOTHING, null=True, blank=True)
     project_work = models.IntegerField()
     quality_of_demo = models.IntegerField()
     project_report = models.IntegerField()
@@ -134,6 +167,7 @@ class Review5(models.Model):
         managed = True
         #db_table = 'review_5'
         unique_together = (('srn', 'fac_id'),)
+
 
 class Student(models.Model):
     srn = models.CharField(primary_key=True, max_length=20)
@@ -146,34 +180,50 @@ class Student(models.Model):
     class Meta:
         managed = True
         constraints = [
-            models.CheckConstraint(name='phone_constraint_student',check=models.Q(phone__startswith='+')),
-            models.CheckConstraint(name='srn_constraint',check=models.Q(srn__startswith='PES')),
+            models.CheckConstraint(
+                name='phone_constraint_student', check=models.Q(phone__startswith='+')),
+            models.CheckConstraint(name='srn_constraint',
+                                   check=models.Q(srn__startswith='PES')),
         ]
         #db_table = 'student'
 
+# use this function to generate team_id always
+# def add_one_team(year_code):
+#     largest = Team.objects.filter(year_code=year_code).order_by('team_id').last()
+#     if not largest:
+#         return str(1).zfill(10)
+#     return str(int(largest.team_id) + 1).zfill(10)
+
+
 class Team(models.Model):
-    team_name = models.CharField(max_length=50)
+    year_code = models.CharField(max_length=10)
+    team_id = models.CharField(max_length=10)
     description = models.CharField(max_length=200, blank=True, null=True)
-    guide = models.ForeignKey(Faculty, models.DO_NOTHING, db_column='guide', blank=True, null=True)
+    guide = models.ForeignKey(
+        Faculty, models.DO_NOTHING, db_column='guide', blank=True, null=True)
     panel = models.ForeignKey(Panel, models.DO_NOTHING, blank=True, null=True)
-    team_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
 
     class Meta:
         managed = True
-        unique_together =(('team_name'),)
+        unique_together = (('year_code', 'team_id'),)
         #db_table = 'team'
+
 
 class TeamFacultyReview(models.Model):
     team = models.ForeignKey(Team, models.DO_NOTHING)
-    fac_id = models.ForeignKey(Faculty, models.DO_NOTHING)
+    fac_id = models.ForeignKey(
+        Faculty, models.DO_NOTHING, null=True, blank=True)
     review_number = models.IntegerField()
     remark = models.CharField(max_length=200, blank=True, null=True)
     id = models.AutoField(primary_key=True)
 
     class Meta:
         constraints = [
-            models.CheckConstraint(name='review_number_gte',check=models.Q(review_number__gte=1)),
-            models.CheckConstraint(name='review_number_lte',check=models.Q(review_number__lte=5))
+            models.CheckConstraint(
+                name='review_number_gte', check=models.Q(review_number__gte=1)),
+            models.CheckConstraint(
+                name='review_number_lte', check=models.Q(review_number__lte=5))
         ]
         managed = True
         #db_table = 'team_faculty_review'
