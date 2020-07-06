51609e20f9dd1a9da41d1d7087f9e8b869193031
Dhruva Kashyap <dhruva12kashyap@gmail.com>
2020-07-06 21:15:48 +0530

    evaluator 90%

--- Back_end/NVRD/eval/templates/eval/scripts/evaluator_evaluations.js
+++ Back_end/NVRD/eval/templates/eval/scripts/evaluator_evaluations.js
@@ -1,4 +1,5 @@
 evaluator_refresh = evaluator_evaluations_refresh
+var lst = ["srn", "name"];
 var time_E = null;
 function evaluator_evaluations_refresh() {
     var xhttp = new XMLHttpRequest()
@@ -44,12 +45,12 @@ function E_evaluations_opentip(e, t) {
     document.getElementById('nav_bar_E').style = "display:none;"
     // str="<table class='table table-striped'><thead><tr><th scope='col'></th><th scope='col'></th><th scope='col'></th><th scope='col'></th></tr></thead><tbody id='C_TFR_R1_tbody'></tbody></table>"
     // document.getElementById('evaluation_views').innerHTML=str
-    console.log(t)
+    // console.log(t)
     var team_year_code = t.children[0].textContent.split("   ")[0];
     var team_id = t.children[0].textContent.split("   ")[1];
     var team_name = t.children[1].children[0].textContent.split(":")[1]
     var team_desc = t.children[1].children[1].textContent.split(":")[1]
-    console.log(team_id, team_year_code, team_name)
+    // console.log(team_id, team_year_code, team_name)
     document.getElementById('evaluation_views_card_header').textContent = team_year_code + "-" + team_id;
     var curr_list = returnCurrentList()
     var review_number = curr_list[2].split("-")[1];
@@ -63,6 +64,7 @@ function E_evaluations_opentip(e, t) {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             let data = JSON.parse(this.responseText)
             console.log(data)
+            Evaluation_format = data
             var thead = ''
             max_vals = {
                 1: [-2, -2, 10, 10, 10, 10, -2, -2, -1, -3],
@@ -71,7 +73,7 @@ function E_evaluations_opentip(e, t) {
                 4: [-2, -2, 10, 10, 10, 10, -2, -2, -1, -3],
                 5: [-2, -2, 10, 10, 10, 10, -2, -2, -1, -3]
             }
-            var lst = ["srn", "name"]
+            lst = ["srn", "name"]
             for (let i of Object.keys(data["individual_review"][0])) {
                 if (lst.indexOf(i) == -1 && ["is_evaluated", "comments", "fac_id"].indexOf(i) == -1) {
                     lst.push(i)
@@ -99,12 +101,12 @@ function E_evaluations_opentip(e, t) {
                     }
                     else if (max_vals[review_number][c] == -1) {
                         tbody += ("<td>" + "<textarea class=\"form-control bg-transparent text-white text-center border-white\" " + i[j] + "></textarea></td>")
-                        }
+                    }
                     else if (max_vals[review_number][c] == -3) {
                         tbody += ("<td><input type=\"checkbox\"></td>")
                         // tbody += ("<td>" + "<label class=\"switch\"><input type='checkbox' default='" + i[j] + "' size='10'><span class=\"slider round\"></span></label></td>")
                         // // tbody += "<td>"+"<span class=\"custom-control custom-switch\"><input type=\"checkbox\" class=\"custom-control-input\"><label class=\"custom-control-label\" for=\"customSwitch1\"></label></span>"+"</td>"
-                    
+
                     }
                     else {
                         tbody += ("<td>" + "<input class=\"form-control bg-transparent text-white text-center\" type='number' value='" + i[j] + "' size='2' min=\"0\" max='" + max_vals[review_number][c] + "'></td>")
@@ -114,17 +116,10 @@ function E_evaluations_opentip(e, t) {
                 tbody += "</tr>"
             }
             document.getElementById("evaluation_views_table_body").innerHTML = tbody
-
-
-            
-
-
-
-
-
+            if (data["team_remarks"])
+                document.getElementById("evaluator_team_remark").innerHTML = data["team_remarks"]
         }
     }
-
     xhttp.open("GET", "/api/" + getCookie("username") + "/" + panel_year_code + "-" + panel_id + "/" + review_number + "/" + team_year_code + "-" + team_id + "/marks-view/", true);
     xhttp.setRequestHeader("Authorization", "Bearer " + getCookie("token"));
     xhttp.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
@@ -177,4 +172,54 @@ function panel_review_refresh_Evaluations() {
     xhttp.setRequestHeader("Authorization", "Bearer " + getCookie("token"));
     xhttp.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
     xhttp.send()
+}
+
+
+function submit_evaluation(e, t) {
+    var team_year_code = document.getElementById("evaluation_views_card_header").innerHTML.split("-")[0]
+    var team_id = document.getElementById("evaluation_views_card_header").innerHTML.split("-")[1]
+    var curr_list = returnCurrentList()
+    var review_number = curr_list[2].split("-")[1];
+    var panel_year_code = curr_list[1].split("-")[0]
+    var panel_id = curr_list[1].split("-")[1]
+    var xhttp = new XMLHttpRequest()
+    var jsonarray = []
+    trows = document.getElementById("evaluation_views_table_body").children
+
+    for(let i of trows)
+        jsonarray.push({})
+    theads=document.getElementById("evaluation_views_table_fields").children
+    for(i of lst)
+    {
+        for(var j=0;j<jsonarray.length;++j)
+            jsonarray[j][i]=''
+    }
+    var c=0;
+    for(let i of trows)
+    {
+        var vals=[]
+        for(let j of i.children)
+            if(j.firstElementChild)
+                vals.push(j.firstElementChild.value)
+            else
+                vals.push(j.textContent)
+        for(var j=0;j<vals.length;++j)
+        {    
+            key=lst[j]
+            jsonarray[c][key]=vals[j]
+        }
+        c++;
+    }
+    var ret={"individul_review":jsonarray,"review_number":review_number,"team":{"team_year_code":team_year_code,"team_id":team_id},"team_remark":document.getElementById("evaluator_team_remark").textContent}
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            console.log(data)
+        }
+    }
+    xhttp.open("POST", "/api/" + getCookie("username") + "/" + panel_year_code + "-" + panel_id + "/" + review_number + "/" + team_year_code + "-" + team_id + "/marks-view/", true);
+    xhttp.setRequestHeader("Authorization", "Bearer " + getCookie("token"));
+    xhttp.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+    xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    xhttp.send(ret);
 }
