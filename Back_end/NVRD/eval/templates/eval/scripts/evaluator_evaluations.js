a2440640b86e2df6cea5ce52bc1c98b0b7732ef8
VikasGowda18 <57809400+VikasGowda18@users.noreply.github.com>
2020-07-06 17:14:17 +0530

    Update evaluator_evaluations.js

--- Back_end/NVRD/eval/templates/eval/scripts/evaluator_evaluations.js
+++ Back_end/NVRD/eval/templates/eval/scripts/evaluator_evaluations.js
@@ -1,4 +1,5 @@
 evaluator_refresh=evaluator_evaluations_refresh
+var time_E=null;
 function evaluator_evaluations_refresh()
 {
     var xhttp = new XMLHttpRequest()
@@ -10,9 +11,14 @@ function evaluator_evaluations_refresh()
                 str += C_team_fac_review_cardmaker(data[i]["team_year_code"], data[i]["team_id"], data[i]["team_name"], data[i]["description"], data[i]["guide"])
             }
             str += '</div>'
-            document.getElementById("evaluation_team_cards").innerHTML = str
-            document.getElementById("evaluation_team_cards").style="display:inline"
-            document.getElementById("evaluation_views").style="display:none"
+            document.getElementById("evaluation_team_cards").innerHTML = str;
+            document.getElementById("evaluation_team_cards").style="display:inline;"
+            document.getElementById("evaluation_views").style="display:none;"
+            if(time_E!=null)
+            {
+                clearInterval(time_E);
+                time_E=null;
+            }
             panel_review_refresh_Evaluations()
         }
     }
@@ -36,8 +42,41 @@ function C_team_fac_review_cardmaker(team_year_code, team_id, team_name, descrip
 
 function E_evaluations_opentip(e,t)
 {   
-    document.getElementById("evaluation_team_cards").style="display:none"
-    document.getElementById("evaluation_views").style="display:inline"
+    document.getElementById("evaluation_team_cards").style="display:none;"
+    document.getElementById("evaluation_views").style="display:inline;"
+    document.getElementById('nav_bar_E').style="display:none;"
+    // str="<table class='table table-striped'><thead><tr><th scope='col'></th><th scope='col'></th><th scope='col'></th><th scope='col'></th></tr></thead><tbody id='C_TFR_R1_tbody'></tbody></table>"
+    // document.getElementById('evaluation_views').innerHTML=str
+    console.log(t)
+    var team_year_code=t.children[0].textContent.split("  ")[0];
+    var team_id=t.children[0].textContent.split("  ")[1];
+    var team_name=t.children[1].children[0].textContent.split(":")[1]
+    console.log(team_id,team_year_code,team_name)
+    document.getElementById('evaluation_views_card_header').textContent= team_year_code + "-"+  team_id;
+    var curr_list=returnCurrentList()
+    var review_number=curr_list[2].split("-")[1];
+    var panel_year_code=curr_list[1].split("-")[0]
+    var panel_id=curr_list[1].split("-")[1]
+    document.getElementById('evaluation_views_card_body_review').textContent=curr_list[2]
+    document.getElementById('evaluation_views_card_body_head').textContent=team_name
+    // var xhttp=new XMLHttpRequest()
+    //     xhttp.onreadystatechange=function(){
+    //     if(this.readyState == 4 && this.status == 201)
+    //     {
+    //             let data=JSON.parse(this.responseText)
+    //             document.getElementById("marks-put").innerHTML="<strong>details</strong>: "+data["detail"]
+    //     } 
+    // let facid=document.getElementById("username").value
+    // let panel_year_code=document.getElementById("panel_year_code").value
+    // let panel_id=document.getElementById("panel_id").value
+    // let team_year_code=document.getElementById("team_year_code").value
+    // let team_id=document.getElementById("team_id").value
+    // console.log(req)    
+    // xhttp.open("PUT","/api/"+ getCookie("username") +"/"+panel_year_code+"-"+panel_id+"-"+team_year_code+"-"+team_id+"-"+review_number+"/marks/", true);
+    // xhttp.setRequestHeader("Authorization", "Bearer "+factoken);
+    // xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    // xhttp.send(JSON.stringify(req));
+    // //xhttp.send(req);
 
 }
 
@@ -72,7 +111,8 @@ function panel_review_refresh_Evaluations() {
              let review_no=curr_list[2].split("-")[1]
              let info=data[review_no-1]
              var open_time=new Date(info["open_time"]),close_time=new Date(info["close_time"])
-             setInterval(time_refresh,60000,open_time,close_time)
+             time_E=setInterval(time_refresh,60000,open_time,close_time)
+             time_refresh(open_time,close_time)
 
       }
     }
