7a3937be7c5ff1165d61643fd19a31122dbd6017
niksram <nikhilsram.off@gmail.com>
2020-07-09 22:20:27 +0530

    download csv made from frontend

--- Back_end/NVRD/eval/templates/eval/scripts/admin_marks_view.js
+++ Back_end/NVRD/eval/templates/eval/scripts/admin_marks_view.js
@@ -1,40 +1,43 @@
 var marks_store_A;
 
-function marks_view_refresh_A() {
-    var xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            let data = JSON.parse(this.responseText)
-            marks_store_A = data
-            var lst = ['srn', 'name', 'team_year_code', 'team_id', 'dept_id']
-            var str = ''
-            for (let i in data) {
-                str += '<tr><td scope="row" style="text-align:right"><input name="marks_view_boxes_A" class="form-check-input position-static" type="checkbox"></input></td>'
-                for (let j in lst) {
-                    str += ("<td>" + data[i][lst[j]] + "</td>")
-                }
-                str += ("<td>" + admin_marks_return_status(data[i]) + "</td>")
-                str += '</tr>'
-            }
-            document.getElementById("marks_view_body_A").innerHTML = str
-            var tbody = document.getElementById("marks_view_body_A")
-            for (i = 0; i < tbody.children.length; i++) {
-                tbody.children[i].addEventListener('click', marks_view_modal_A)
-            }
-            checkerInit("marks_view_boxes_main_A", "marks_view_boxes_A")
-        }
-    }
-    refreshLoader(xhttp)
-    xhttp.open("GET", "/api/" + getCookie("username") + "/marks-view/", true);
-    xhttp.setRequestHeader("Authorization", "Bearer " + getCookie("token"));
-    xhttp.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
-    xhttp.send();
-}
+// function marks_view_refresh_A() {
+//     document.getElementById("marks_view_initial_A").setAttribute('style','display:none')
+//     var xhttp = new XMLHttpRequest()
+//     xhttp.onreadystatechange = function () {
+//         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+//             let data = JSON.parse(this.responseText)
+//             marks_store_A = data
+//             var lst = ['srn', 'name', 'team_year_code', 'team_id', 'dept_id']
+//             var str = ''
+//             for (let i in data) {
+//                 str += '<tr><td scope="row" style="text-align:right"><input name="marks_view_boxes_A" class="form-check-input position-static" type="checkbox"></input></td>'
+//                 for (let j in lst) {
+//                     str += ("<td>" + data[i][lst[j]] + "</td>")
+//                 }
+//                 str += ("<td>" + admin_marks_return_status(data[i]) + "</td>")
+//                 str += '</tr>'
+//             }
+//             document.getElementById("marks_view_body_A").innerHTML = str
+//             var tbody = document.getElementById("marks_view_body_A")
+//             for (i = 0; i < tbody.children.length; i++) {
+//                 tbody.children[i].addEventListener('click', marks_view_modal_A)
+//             }
+//             checkerInit("marks_view_boxes_main_A", "marks_view_boxes_A")
+//         }
+//     }
+//     refreshLoader(xhttp)
+//     xhttp.open("GET", "/api/" + getCookie("username") + "/marks-view/", true);
+//     xhttp.setRequestHeader("Authorization", "Bearer " + getCookie("token"));
+//     xhttp.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+//     xhttp.send();
+// }
 
 function marks_view_search_A() {
+    document.getElementById("marks_view_initial_A").setAttribute('style', 'display:none')
     var tbody
     var xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = function () {
+        console.log('loading', this.readyState, this.LOADING)
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             let data = JSON.parse(this.responseText)
             marks_store_A = data
@@ -146,9 +149,9 @@ function marks_view_modal_A(e) {
             rows[c_c].children[3].innerHTML = i["percentage"].toFixed(2) + "%";
             c_c++
         }
-        rows[c_c].children[1].innerHTML = '<b>'+val["review"]["total"]["marks_scored"]+'</b>';
-        rows[c_c].children[2].innerHTML = '<b>'+val["review"]["total"]["total_marks"]+'</b>';
-        rows[c_c].children[3].innerHTML = '<b>'+val["review"]["total"]["percentage"].toFixed(2) + "%"+'</b>';
+        rows[c_c].children[1].innerHTML = '<b>' + val["review"]["total"]["marks_scored"] + '</b>';
+        rows[c_c].children[2].innerHTML = '<b>' + val["review"]["total"]["total_marks"] + '</b>';
+        rows[c_c].children[3].innerHTML = '<b>' + val["review"]["total"]["percentage"].toFixed(2) + "%" + '</b>';
         $('#modal_admin_marks_view').modal({
             show: true
         })
@@ -194,21 +197,23 @@ function getCheckedBoxes_A_marksview(chkboxName) {
     return checkboxesChecked.length > 0 ? checkboxesChecked : null;
 }
 function Admin_print() {
-    var xhttp = new XMLHttpRequest()
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            download("results.csv", this.responseText);
-        }
-    }
     var students = getCheckedBoxes_A_marksview("marks_view_boxes_A")
-    refreshLoader(xhttp)
-    xhttp.open("POST", "/api/" + getCookie("username") + "/file/", true);
-    xhttp.setRequestHeader("Authorization", "Bearer " + getCookie("token"));
-    xhttp.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
-    xhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-    console.log(students)
-    if(students && students.length)
-        xhttp.send(JSON.stringify(students));
+    var mainstr = "SRN,Name,Review 1(40),Review 2(40),Review 3(35),Review 4(40),Review 5(40),Total(195),Percentage,\n"
+    if (students && students.length) {
+        for (let i of marks_store_A) {
+            if (students.indexOf(i["srn"])!=-1) {
+                var str = i["srn"]+','+i["name"]+','
+                for (j of i["review"]["total_individual"]) {
+                    str += (j["marks_scored"] + ',')
+                }
+                str += (i["review"]["total"]["marks_scored"] + ',' + i["review"]["total"]["percentage"].toFixed(2) + ',' + '\n')
+                mainstr += str
+            }
+        }
+        download("results.csv", mainstr);
+    }
 }
 
-admin_refresh = marks_view_refresh_A
+admin_refresh = function () {
+    var a = 10
+}
